d208f9ce6a37456793188729ccd1f07efcb0efad
Pouriya <pouriya.jamshidi@gmail.com>
2022-07-17 23:39:13 +0200

    add preliminary IPv6 support and few fixes to IPv4 analyzer

--- fwmonitor.py
+++ fwmonitor.py
@@ -18,9 +18,7 @@ class TXTColor:
     BOLD = "\033[1m"
 
 
-class IPv4:
-    SRC_IPV4_PTRN = re.compile(r"SRC=([0-9]{1,3}[\.]){3}[0-9]{1,3}")
-    DST_IPV4_PTRN = re.compile(r"DST=([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+class L4:
     PROTO_PTRN = re.compile(r"PROTO=\S*")
     SRC_PORT_PTRN = re.compile(r"SPT=\d*")
     DST_PORT_PTRN = re.compile(r"DPT=\d*")
@@ -28,6 +26,16 @@ class IPv4:
     TTL_PTRN = re.compile(r"TTL=\d*")
     LOG_DATE_PTRN = re.compile(r"^...\s* \d* \d\d:\d\d:\d\d")
     PKT_TYPE_PTRN = re.compile(r"RES=.* ... ")
+
+
+class IPv4(L4):
+    SRC_IPV4_PTRN = re.compile(r"SRC=([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+    DST_IPV4_PTRN = re.compile(r"DST=([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+
+
+class IPv6(L4):
+    SRC_IPV6_PTRN = re.compile(r"SRC=([0-9a-fA-F:]+)")
+    DST_IPV6_PTRN = re.compile(r"DST=([0-9a-fA-F:]+)")
 
 
 def sig_handler(frame, signal):
@@ -74,12 +82,12 @@ def check_file(logfile):
     if not Path(logfile).is_file():
         print(f"{TXTColor.FAIL}", end="")
         print(f"[-] Cannot find {logfile}")
-        print(f"{TXTColor.END}", end="")
+        print(f"{TXTColor.END}")
         sys.exit(1)
 
 
-def read_file(file):
-    with open(file, "r", errors="ignore") as f:
+def read_file(file_name):
+    with open(file_name, "r", encoding="utf-8", errors="ignore") as f:
         lines = f.readlines()
     return lines
 
@@ -167,8 +175,92 @@ def analyze_ipv4_log(log, key):
 
         log_date = re.search(IPv4.LOG_DATE_PTRN, line)[0]
 
+        spacing = 0
         print(f"{TXTColor.BOLD}", end="")
-        print(f"{counter})\t", end="")
+        print(f"{counter})", end="")
+        spacing += len(str(counter))
+        print(" " * (7 - spacing), end="")
+        print(f"{src_ip}\t", end="")
+        print(f"{src_port}\t\t", end="")
+        print(f"{dst_ip}\t  ", end="")
+        print(f"{dst_port}\t\t\t", end="")
+
+        if protocol != "TCP":
+            print(f"{protocol}", end="")
+            print(" " * (16 - len(protocol)), end="")
+        else:
+            print(f"{protocol}", end="")
+            print(f"{pkt_type}", end="")
+            print(" " * (13 - len(pkt_type)), end="")
+
+        print(f"{pkt_len}\t", end="")
+        print(f"{ttl}\t", end="")
+        print(f"{log_date}", end="")
+        print(" " * (130 - (len(log_date) + 113)) + f"{TXTColor.GREEN}|")
+        print("-" * 130)
+        print(f"{TXTColor.END}", end="")
+
+    return found, scanned_lines
+
+
+def analyze_ipv6_log(log, key):
+    counter = 0
+    scanned_lines = 0
+    found = False
+
+    for line in log:
+        scanned_lines += 1
+
+        if key not in line or not re.search(IPv6.SRC_IPV6_PTRN, line):
+            continue
+
+        found = True
+        counter += 1
+
+        if counter == 1 or (counter % 20) == 0:
+            print_banner()
+
+        src_ip_raw = re.search(IPv6.SRC_IPV6_PTRN, line)[0]
+        src_ip = src_ip_raw.replace("SRC=", "")
+
+        dst_ip_raw = re.search(IPv6.DST_IPV6_PTRN, line)[0]
+        dst_ip = dst_ip_raw.replace("DST=", "")
+
+        protocol_raw = re.search(IPv6.PROTO_PTRN, line)[0]
+        protocol = protocol_raw.replace("PROTO=", "")
+
+        src_port = dst_port = None
+
+        # let's see if we need to look for src/dst ports
+        if protocol in ["TCP", "UDP"]:
+            src_port_raw = re.search(IPv6.SRC_PORT_PTRN, line)[0]
+            src_port = src_port_raw.replace("SPT=", "")
+            dst_port_raw = re.search(IPv6.DST_PORT_PTRN, line)[0]
+            dst_port = dst_port_raw.replace("DPT=", "")
+            # TCP has packet type unlike UDP
+            if protocol == "TCP":
+                pkt_type_raw = re.search(IPv6.PKT_TYPE_PTRN, line)[0]
+                pkt_type = pkt_type_raw.replace("RES=0x00 ", "/")
+        elif protocol in ["ICM", "ICMP"]:
+            protocol = "ICMP"
+        elif protocol == "2":
+            protocol = "IGMP"
+
+        pkt_len = re.search(IPv6.PKT_LEN_PTRN, line)[0]
+        pkt_len = pkt_len.replace("LEN=", "")
+
+        ttl_raw = re.search(IPv6.TTL_PTRN, line)[0]
+        ttl = ttl_raw.replace("TTL=", "")
+
+        log_date = re.search(IPv6.LOG_DATE_PTRN, line)[0]
+
+        spacing = 0
+        print(f"{TXTColor.BOLD}", end="")
+        # print(f"{counter})\t", end="")
+        # print(f"{src_ip}\t", end="")
+        print(f"{counter})", end="")
+        spacing += len(str(counter))
+        print(" " * (7 - spacing), end="")
         print(f"{src_ip}\t", end="")
         print(f"{src_port}\t\t", end="")
         print(f"{dst_ip}\t  ", end="")
@@ -202,7 +294,7 @@ def main():
     print(f"{TXTColor.END}", end="")
 
     while True:
-        log = read_file(logfile)   # Tackles log rotation
+        log = read_file(logfile)  # Tackles log rotation
         result, scanned_lines = analyze_ipv4_log(log, key)
         display_scanned_lines(scanned_lines)
         if not result:
