a8b11167a2433c24e6feb26e60bd2e2dd2fab27b
Pouriya <pouriya.jamshidi@gmail.com>
2022-08-24 18:50:55 +0200

    add initial IPv6 support fix to Regex patterns fix IPv6 flag

--- fwmonitor.py
+++ fwmonitor.py
@@ -26,7 +26,7 @@ class TransportLayer:
     PKT_LEN_PTRN = re.compile(r"LEN=\d*")
     TTL_PTRN = re.compile(r"TTL=\d*")
     LOG_DATE_PTRN = re.compile(r"^...\s* \d* \d\d:\d\d:\d\d")
-    PKT_TYPE_PTRN = re.compile(r"RES=.* ... ")
+    PKT_TYPE_PTRN = re.compile(r"RES=.*\s...\s")
 
 
 class IPv4(TransportLayer):
@@ -35,8 +35,9 @@ class IPv4(TransportLayer):
 
 
 class IPv6(TransportLayer):
-    SRC_IPV6_PTRN = re.compile(r"SRC=([0-9a-fA-F:]+)")
-    DST_IPV6_PTRN = re.compile(r"DST=([0-9a-fA-F:]+)")
+    SRC_IPV6_PTRN = re.compile(r"SRC=([0-9a-f]{1,4}:+){3,7}[0-9a-f]{1,4}")
+    DST_IPV6_PTRN = re.compile(r"DST=([0-9a-f]{1,4}:+){3,7}[0-9a-f]{1,4}")
+    TTL_PTRN = re.compile(r"HOPLIMIT=\d*")
 
 
 def sig_handler(frame, signal) -> NoReturn:
@@ -62,7 +63,9 @@ def get_user_input() -> Namespace:
         help="interval to check the file in seconds. Default is 60"
         + ". specify '0' to run once",
     )
-    parser.add_argument("--ipv6", default=False, help="scan for IPv6 logs")
+    parser.add_argument(
+        "--ipv6", default=False, action="store_true", help="scan IPv6 logs"
+    )
 
     return parser.parse_args()
 
@@ -202,68 +205,43 @@ def analyze_ipv6_log(logs: List[str], key: str) -> Tuple[bool, int]:
         found = True
         counter += 1
 
-        if counter == 1 or (counter % 20) == 0:
-            print_banner()
-
-        src_ip_raw = re.search(IPv6.SRC_IPV6_PTRN, line)[0]
-        src_ip = src_ip_raw.replace("SRC=", "")
-
-        dst_ip_raw = re.search(IPv6.DST_IPV6_PTRN, line)[0]
-        dst_ip = dst_ip_raw.replace("DST=", "")
-
-        protocol_raw = re.search(IPv6.PROTO_PTRN, line)[0]
-        protocol = protocol_raw.replace("PROTO=", "")
-
+        src_ip = re.search(IPv6.SRC_IPV6_PTRN, line)[0].replace("SRC=", "")
+        dst_ip = re.search(IPv6.DST_IPV6_PTRN, line)[0].replace("DST=", "")
+        protocol = re.search(IPv6.PROTO_PTRN, line)[0].replace("PROTO=", "")
         src_port = dst_port = None
 
         # let's see if we need to look for src/dst ports
         if protocol in ["TCP", "UDP"]:
-            src_port_raw = re.search(IPv6.SRC_PORT_PTRN, line)[0]
-            src_port = src_port_raw.replace("SPT=", "")
-            dst_port_raw = re.search(IPv6.DST_PORT_PTRN, line)[0]
-            dst_port = dst_port_raw.replace("DPT=", "")
+            src_port = re.search(IPv6.SRC_PORT_PTRN, line)[0].replace("SPT=", "")
+            dst_port = re.search(IPv6.DST_PORT_PTRN, line)[0].replace("DPT=", "")
             # TCP has packet type unlike UDP
             if protocol == "TCP":
-                pkt_type_raw = re.search(IPv6.PKT_TYPE_PTRN, line)[0]
-                pkt_type = pkt_type_raw.replace("RES=0x00 ", "/")
+                pkt_type = re.search(IPv6.PKT_TYPE_PTRN, line)[0].replace(
+                    "RES=0x00 ", ""
+                )
         elif protocol in ["ICM", "ICMP"]:
             protocol = "ICMP"
         elif protocol == "2":
             protocol = "IGMP"
 
-        pkt_len = re.search(IPv6.PKT_LEN_PTRN, line)[0]
-        pkt_len = pkt_len.replace("LEN=", "")
-
-        ttl_raw = re.search(IPv6.TTL_PTRN, line)[0]
-        ttl = ttl_raw.replace("TTL=", "")
-
+        pkt_len = re.search(IPv6.PKT_LEN_PTRN, line)[0].replace("LEN=", "")
+        ttl = re.search(IPv6.TTL_PTRN, line)[0].replace("HOPLIMIT=", "")
         log_date = re.search(IPv6.LOG_DATE_PTRN, line)[0]
 
-        spacing = 0
-        print(f"{TXTColor.BOLD}", end="")
-        # print(f"{counter})\t", end="")
-        # print(f"{src_ip}\t", end="")
-        print(f"{counter})", end="")
-        spacing += len(str(counter))
-        print(" " * (7 - spacing), end="")
-        print(f"{src_ip}\t", end="")
-        print(f"{src_port}\t\t", end="")
-        print(f"{dst_ip}\t  ", end="")
-        print(f"{dst_port}\t\t\t", end="")
+        log = f"{counter})\nSource IP: {src_ip}\nSource Port: {src_port}\n"
+        log += f"Destination IP: {dst_ip}\nDestination Port: {dst_port}\n"
 
         if protocol != "TCP":
-            print(f"{protocol}", end="")
-            print(" " * (16 - len(protocol)), end="")
+            log += f"Protocol: {protocol}\n"
         else:
-            print(f"{protocol}", end="")
-            print(f"{pkt_type}", end="")
-            print(" " * (13 - len(pkt_type)), end="")
+            log += f"Protocol: {protocol}\nPacket Type: {pkt_type}\n"
 
-        print(f"{pkt_len}\t", end="")
-        print(f"{ttl}\t", end="")
-        print(f"{log_date}", end="")
-        print(" " * (130 - (len(log_date) + 113)) + f"{TXTColor.GREEN}|")
-        print("-" * 130)
+        log += f"Packet Length: {pkt_len}\nTTL: {ttl}\nDate: {log_date}\n"
+
+        print(f"{TXTColor.BOLD}", end="")
+        print(f"{TXTColor.GREEN}-" * 80)
+        print(f"{TXTColor.END}{log}")
+        print(f"{TXTColor.GREEN}-" * 80)
         print(f"{TXTColor.END}", end="")
 
     return found, scanned_lines
