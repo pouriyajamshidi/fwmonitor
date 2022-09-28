cb7bfb7b81a338eb6e7a0bde4632ac572086ff3c
Pouriya <pouriya.jamshidi@gmail.com>
2022-09-28 18:43:51 +0200

    fix Pypi markdown issue

--- fwmonitor.py
+++ fwmonitor.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from time import sleep
 from typing import List, NoReturn, Tuple
 
-__version__ = "1.2.1"
+__version__ = "1.2.3"
 
 
 class TXTColor:
--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="fwmonitor",
-    version="1.2.1",
+    version="1.2.3",
     author="Pouriya Jamshidi",
     scripts=["fwmonitor"],
     description="for network traffic analysis, displays your iptables, UFW, or any application that logs in the same format, in a pleasant way.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
